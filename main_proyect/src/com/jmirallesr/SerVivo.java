595034fba9fbc40f7b620d45f3db0ed1206bd8fc
Adrian Carneiro <acarneirod@fpcoruna.afundacion.org>
2021-10-18 18:03:19 +0200

    Creacion clase SerVivo Solo contiene atributosbasicos,getters,setters y un constructor basico

---
+++ main_proyect/src/com/jmirallesr/SerVivo.java
@@ -0,0 +1,90 @@
+package com.company;
+
+public class SerVivo {
+    private String id,especie,genero,fechaCompra,procedencia,comportamiento,estado,alimentacion,idSimbiosis,tratamiento;
+    private Boolean simbiosis;
+
+    SerVivo(){}
+    SerVivo(String id,String especie,String genero,String fechaCompra,String procedencia,String comportamiento,String estado,String alimentacion){
+        this.id=id;
+        this.especie=especie;
+        this.genero=genero;
+        this.fechaCompra=fechaCompra;
+        this.procedencia=procedencia;
+        this.comportamiento=comportamiento;
+        this.estado=estado;
+        this.alimentacion=alimentacion;
+
+    }
+
+    public void comprobacion(){
+
+    }
+
+    public String getId() {
+        return id;
+    }
+    public String getEspecie() {
+        return especie;
+    }
+    public String getGenero() {
+        return genero;
+    }
+    public String getFechaCompra() {
+        return fechaCompra;
+    }
+    public String getProcedencia() {
+        return procedencia;
+    }
+    public String getComportamiento() {
+        return comportamiento;
+    }
+    public String getEstado() {
+        return estado;
+    }
+    public String getAlimentacion() {
+        return alimentacion;
+    }
+    public String getIdSimbiosis() {
+        return idSimbiosis;
+    }
+    public String getTratamiento() {
+        return tratamiento;
+    }
+    public Boolean getSimbiosis() {
+        return simbiosis;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public void setEspecie(String especie) {
+        this.especie = especie;
+    }
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+    public void setFechaCompra(String fechaCompra) {
+        this.fechaCompra = fechaCompra;
+    }
+    public void setProcedencia(String procedencia) {
+        this.procedencia = procedencia;
+    }
+    public void setComportamiento(String comportamiento) {
+        this.comportamiento = comportamiento;
+    }
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    public void setAlimentacion(String alimentacion) {
+        this.alimentacion = alimentacion;
+    }
+    public void setIdSimbiosis(String idSimbiosis) {
+        this.idSimbiosis = idSimbiosis;
+    }
+    public void setTratamiento(String tratamiento) {
+        this.tratamiento = tratamiento;
+    }
+    public void setSimbiosis(Boolean simbiosis) {
+        this.simbiosis = simbiosis;
+    }
+}
