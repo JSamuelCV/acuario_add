2ded7323ceafaa0fab76955b10c901d33dd68e00
enriquerivasf <erivasf@fpcoruna.afundacion.org>
2021-10-18 21:08:05 +0200

    Creacion clase Clientes

---
+++ main_proyect/src/com/jmirallesr/Clientes.java
@@ -0,0 +1,31 @@
+package com.jmirallesr;
+/*
+clientes:
+   1- entrada:
+                registrodedatos(metodos)
+
+                    nombre
+                    apellidos
+                    Dni
+                    correo electronico
+                    nombre responsable(opcional)
+                    hora de entrada:
+
+    2- salida cliente:
+                    mostrarSalida(metodo)
+                        hora de salida
+                        horas que ha estado
+                        valoracion(0-5)
+    3- valoracion
+                    mostrarvaloracion()
+                        individual(todas)
+                        media
+    4-  estadistica
+                   mostrarEstadistica()
+                        Nº de gente en un dia
+
+
+ */
+public class Clientes {
+
+}
