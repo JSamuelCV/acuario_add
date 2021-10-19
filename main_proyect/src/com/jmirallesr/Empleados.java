a4ea9d3fc1ee70e6cf4acb0bca8583915d990eee
45NGC <79377468+45NGC@users.noreply.github.com>
2021-10-19 14:10:25 +0200

    Update Empleados + Clase Menus

--- main_proyect/src/com/jmirallesr/Empleados.java
+++ main_proyect/src/com/jmirallesr/Empleados.java
@@ -11,6 +11,7 @@ import java.io.IOException;
                                     - Mantenimiento
                                     - Cuidadores
                                     - Recepcionistas
+            - Cambiar datos empleado
 
             - Añadir empleado
 
@@ -20,57 +21,55 @@ import java.io.IOException;
 public class Empleados {
     public static void main(String[]args) throws IOException {
         Teclado entrada=new Teclado();
+        Menus menu=new Menus();
         boolean fin=false;
         int opcionMenuEmpleados;
         do{
-            opcionMenuEmpleados=menuEmpleados(entrada);
+            opcionMenuEmpleados=Menus.menuEmpleados(entrada);
             switch (opcionMenuEmpleados){
                 case 0 -> fin=true;
-                case 1 -> consultarEmpleado(entrada);
-                case 2 -> anadirEmpleado(entrada);
-                case 3 -> eliminarEmpleado(entrada);
+                case 1 -> consultas(entrada);
+                case 2 -> cambiarDatosEmpleado(entrada);
+                case 3 -> anadirEmpleado(entrada);
+                case 4 -> eliminarEmpleado(entrada);
             }
         }while (!fin);
     }
 
-    static int menuEmpleados(Teclado entrada)throws IOException{
-        int opcion;
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println("MENU EMPLEADOS");
-        System.out.println();
-        System.out.println("1- Consultar empleados");
-        System.out.println("2- Añadir empleado");
-        System.out.println("3- Eliminar empleado");
-        System.out.println();
+    static void consultas(Teclado entrada) throws IOException{
+        boolean fin=false;
+        int opcionMenuConsultas;
         do{
-            System.out.println("Seleccione una opcion (0 para regresar al menu principal) : ");
-            opcion=entrada.leerInt();
-        }while(opcion<0 || opcion>3);
-        return opcion;
+            opcionMenuConsultas=Menus.menuConsultas(entrada);
+            switch (opcionMenuConsultas){
+                case 0 -> fin=true;
+                case 1 -> consultaGeneral(entrada);
+                case 2 -> consultaTipoTrabajo(entrada, opcionMenuConsultas);
+                case 3 -> consultaTipoTrabajo(entrada, opcionMenuConsultas);
+                case 4 -> consultaTipoTrabajo(entrada, opcionMenuConsultas);
+                case 5 -> consultaNumeroEmpleado(entrada);
+            }
+        }while (!fin);
     }
 
-    static int menuConsultas(Teclado entrada)throws IOException{
-        int opcion;
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println("MENU CONSULTAS");
-        System.out.println();
-        System.out.println("1- General");
-        System.out.println("2- Guias");
-        System.out.println("3- Recepcionistas");
-        System.out.println("4- Mantenimiento");
-        System.out.println();
-        do{
-            System.out.println("Seleccione una opcion (0 para regresar al menu principal) : ");
-            opcion=entrada.leerInt();
-        }while(opcion<0 || opcion>3);
-        return opcion;
-    }
 
-    static void consultarEmpleado(Teclado entrada){
+    static void cambiarDatosEmpleado(Teclado entrada) throws IOException{
+        boolean fin=false;
+        int opcionMenuCambiarDatos, numeroEmpleado;
+        // El usuario introduce el numero del usuario que va a alterar
+        do{
+            System.out.println();
+            System.out.println("Introduza el ID del empleado en el que se aplicaran los cambios (0 para regresar al menu principal): ");
+            numeroEmpleado= entrada.leerInt();
+        }while(numeroEmpleado==Integer.MIN_VALUE || numeroEmpleado<0/*|| el empleado no existe*/);
+
+        // En caso de que el empleado exista se le pregunta al usuario lo que desea cambiar
+        do{
+            opcionMenuCambiarDatos=Menus.menuCambiarDatos(entrada);
+            switch (opcionMenuCambiarDatos){
+                case 0 -> fin=true;
+            }
+        }while (!fin);
 
     }
 
@@ -79,6 +78,28 @@ public class Empleados {
     }
 
     static void eliminarEmpleado(Teclado entrada){
+
+    }
+
+
+    // TIPOS DE CONSULTAS:
+
+    static void consultaGeneral(Teclado entrada){
+
+    }
+
+    static void consultaTipoTrabajo(Teclado entrada, int tipoTrabajo){
+        // En funcion del tipo de trabajo que se seleccione cambia el String
+        String trabajoSeleccionado;
+        switch (tipoTrabajo){
+            case 2 -> trabajoSeleccionado="Guias";
+            case 3 -> trabajoSeleccionado="Recepcionistas";
+            case 4 -> trabajoSeleccionado="Mantenimiento";
+        }
+
+    }
+
+    static void consultaNumeroEmpleado(Teclado entrada){
 
     }
 }
